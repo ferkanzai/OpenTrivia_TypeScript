ebb14e815d0e3f659e7a29a7146cff5ee2b57746
Fernando Carmona <fernando.carmona@gruposantander.com>
2021-03-20 17:01:20 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,20 @@ import React, { ReactElement } from 'react';
 import './App.css';
 import { useForm } from "react-hook-form";
 
+import { categories } from './constants'
 
+const categoryKeys: string[] = Object.keys(categories)
 
 const App:React.FC = ():ReactElement =>{
 
-
-
   const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
+  const onSubmit = (data: any) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input name="category" ref={register} />
       <select name="category" ref={register}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">other</option>
+        {categoryKeys.map((category: any) => <option value={categoryKeys[category]}>{category}</option>)}
       </select>
       <input type="submit" />
     </form>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const categories = {
+export type categories = {
   "General Knowledge": 9,
   "Entertainment: Books": 10,
   "Entertainment: Film": 11,
@@ -25,3 +25,29 @@ export const categories = {
   "Entertainment: Cartoon & Animations": 32,
 }
 
+export enum Categories {
+  General_Knowledge = 9,
+  Entertainment_Books = 10,
+  Entertainment_Film = 11,
+  Entertainment_Music = 12,
+  Entertainment_Musicals_Theatres = 13,
+  Entertainment_Television = 14,
+  Entertainment_Video_Games = 15,
+  Entertainment_Board_Games = 16,
+  Science_Nature = 17,
+  Science_Computers = 18,
+  Science_Mathematics = 19,
+  Mythology = 20,
+  Sports = 21,
+  Geography = 22,
+  History_ = 23,
+  Politics = 24,
+  Art = 25,
+  Celebrities = 26,
+  Animals = 27,
+  Vehicles = 28,
+  Entertainment_Comics = 29,
+  Science_Gadgets = 30,
+  Entertainment_Japanese_Anime_Manga = 31,
+  Entertainment_Cartoon_Animations = 32,
+}
