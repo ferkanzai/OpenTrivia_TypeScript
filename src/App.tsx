c0315ef89a69caebfa1406a32d28e89bcf23de25
Isabelgarcia <isabelgmegino@gmail.com>
2021-03-23 21:14:22 +0100

    get function added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { useForm } from "react-hook-form";
 
 import { categories } from './constants'
+import { ContextTrivial } from './context'
 
 interface IFormTrivial {
   questions: number;
@@ -18,23 +19,67 @@ const difficulties = [
   'hard'
 ]
 
-const App:React.FC = ():ReactElement =>{
+const getQuiz = (questionsNumber, category, difficulty) => {
+  const url = 'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium'
+  
+}
+
+
+//
+const getPokemon = (pokemonItem) => {
+
+  console.log("get pokemon =>", pokemonItem)
+
+  const pokemonFormatted = pokemonItem.toLowerCase()
+
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonFormatted}`
+
+  console.log(pokemonFormatted)
+
+
+  return fetch(url, { method: 'GET' })
+    .then((res) => res.json())
+    .then((result) => {
+
+      //Aquí debemos poner un control de pokemon existente o error de la api?
+      return result
+    })
+
+    .catch((error) => {
+      alert('Pokemon no existe')
+      console.log("catch error del fetch")
+      return null
+
+    })
+}
+//
+const App: React.FC = (): ReactElement => {
 
   const { register, handleSubmit } = useForm<IFormTrivial>();
   const onSubmit = (data: IFormTrivial) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor='questions'>Number of questions</label>
-      <input name="questions" id='questions' ref={register} />
-      <select name="category" ref={register}>
-        {categoryKeys.map((category: any) => <option value={categoryKeys[category]}>{category}</option>)}
-      </select>
-      <select name='difficulty' ref={register}>
-        {difficulties.map((difficulty: string) => <option value={difficulty}>{difficulty}</option>)}
-      </select>
-      <input type="submit" />
-    </form>
+    <div>
+
+      <ContextTrivial.Provider value={}>
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor='questions'>Number of questions</label>
+            <input name="questions" id='questions' ref={register} />
+            <select name="category" ref={register}>
+              {categoryKeys.map((category: any) => <option value={categoryKeys[category]}>{category}</option>)}
+            </select>
+            <select name='difficulty' ref={register}>
+              {difficulties.map((difficulty: string) => <option value={difficulty}>{difficulty}</option>)}
+            </select>
+            <input type="submit" />
+          </form>
+        </div>
+
+
+      </ContextTrivial.Provider>
+
+    </div>
   );
 }
 
---
+++ src/context/index.js
@@ -0,0 +1,2 @@
+import { createContext } from 'react'
+export const ContextTrivial = createContext(null)
